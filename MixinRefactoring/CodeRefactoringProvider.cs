fdf3d54c51c208efa36d0c04921de4f75415d9c2
pgenfer <pgenfer@gmail.com>
2016-02-26 23:08:05 +0100

    fix #2: if no members to include, the refactoring is not available

--- MixinRefactoring/CodeRefactoringProvider.cs
+++ MixinRefactoring/CodeRefactoringProvider.cs
@@ -5,8 +5,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.CSharp.Formatting;
 
 namespace MixinRefactoring
 {
@@ -16,24 +14,20 @@ namespace MixinRefactoring
         public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
             // Find the node at the selection.
             var node = root.FindNode(context.Span);
-
             // Only offer a refactoring if the selected node is a type declaration node.
-
             var fieldDeclarationNode = GetContainingFieldDeclaration(node);
             if (fieldDeclarationNode == null)
                 return;
-
-            // try to extract mixin information from the field declaration
             var model = await context.Document.GetSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
-            var mixin = new MixinReferenceFactory(model).Create(fieldDeclarationNode);
-            if (mixin == null)
+            // check if mixin could be executed
+            var mixinCommand = new MixinCommand(model, fieldDeclarationNode);
+            if (!mixinCommand.CanExecute())
                 return;
-
-            var action = CodeAction.Create($"Include mixin: '{mixin.Name}'", c => CreateMixin(context.Document, fieldDeclarationNode, c));
-
+            var action = CodeAction.Create(
+                $"Include mixin: '{mixinCommand.MixinFieldName}'", 
+                c => CreateMixin(context.Document, mixinCommand, c));
             // Register this code action.
             context.RegisterRefactoring(action);
         }
@@ -45,31 +39,19 @@ namespace MixinRefactoring
             return node as FieldDeclarationSyntax;
         }
 
-        private async Task<Solution> CreateMixin(Document document, FieldDeclarationSyntax fieldDeclarationNode, CancellationToken cancellationToken)
+        private async Task<Solution> CreateMixin(Document document, MixinCommand mixinCommand, CancellationToken cancellationToken)
         {
             var model = await document.GetSemanticModelAsync(cancellationToken);
+            if (mixinCommand.CanExecute())
+            {
+                var newClassDeclaration = mixinCommand.Execute();
+                var root = await model.SyntaxTree.GetRootAsync(cancellationToken);
+                var newRoot = root.ReplaceNode(mixinCommand.OriginalChildSource, newClassDeclaration);
+                var newDocument = document.WithSyntaxRoot(newRoot);
+                return newDocument.Project.Solution;
+            }
 
-            var classDeclarationNode = fieldDeclarationNode.Parent as ClassDeclarationSyntax;
-
-            var mixin = new MixinReferenceFactory(model).Create(fieldDeclarationNode);
-            if (mixin == null)
-                return null;
-
-            var mixinChild = new ClassFactory(model).Create(classDeclarationNode);
-            if (mixinChild == null)
-                return null;
-
-            // do the refactoring
-            var mixer = new Mixer();
-            mixer.IncludeMixinInChild(mixin, mixinChild);
-            var syntaxWriter = new IncludeMixinSyntaxWriter(mixer.MembersToImplement, mixin.Name, model);
-            var newClassDeclaration = syntaxWriter.Visit(mixinChild.SourceCode);
-
-            var root = await model.SyntaxTree.GetRootAsync(cancellationToken);
-            var newRoot = root.ReplaceNode(classDeclarationNode, newClassDeclaration);
-
-            var newDocument = document.WithSyntaxRoot(newRoot);
-            return newDocument.Project.Solution;
+            return null;
         }
     }
 }
